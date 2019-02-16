a23bcb63fc7143875d13c54464e68b89a370fc77
Popeye Zhong <9555843@qq.com>
2019-02-17 00:58:27 +0800

    完善更新语句访问器的实现。 :chart_with_upwards_trend: :performing_arts:

--- src/Common/Expressions/UpdateStatementVisitor.cs
+++ src/Common/Expressions/UpdateStatementVisitor.cs
@@ -47,34 +47,88 @@ namespace Zongsoft.Data.Common.Expressions
 		#region 重写方法
 		protected override void OnVisit(IExpressionVisitor visitor, UpdateStatement statement)
 		{
+			if(statement.Tables == null || statement.Tables.Count == 0)
+				throw new DataException("Missing required tables in the update statement.");
+
 			if(statement.Fields == null || statement.Fields.Count == 0)
 				throw new DataException("Missing required fields in the update statment.");
 
 			visitor.Output.Append("UPDATE ");
-			visitor.Visit(statement.Table);
-			visitor.Output.Append(" SET ");
+
+			//依次输出更新的表标识
+			for(int i = 0; i < statement.Tables.Count; i++)
+			{
+				if(i > 0)
+					visitor.Output.Append(",");
+
+				visitor.Visit(statement.Tables[i]);
+			}
+
+			visitor.Output.AppendLine(" SET");
+
+			var index = 0;
 
 			foreach(var field in statement.Fields)
 			{
+				if(index++ > 0)
+					visitor.Output.AppendLine(",");
+
 				visitor.Visit(field.Field);
 				visitor.Output.Append("=");
 				visitor.Visit(field.Value);
 			}
 
+			if(statement.From != null && statement.From.Count > 0)
+				this.VisitFrom(visitor, statement.From);
+
 			if(statement.Where != null)
 				this.VisitWhere(visitor, statement.Where);
-		}
 
-		protected override void OnVisited(IExpressionVisitor visitor, UpdateStatement statement)
-		{
+			if(statement.Returning != null)
+				this.VisitReturning(visitor, statement.Returning);
+
 			visitor.Output.AppendLine(";");
-
-			//调用基类同名方法
-			base.OnVisited(visitor, statement);
 		}
 		#endregion
 
 		#region 虚拟方法
+		protected virtual void VisitReturning(IExpressionVisitor visitor, ReturningClause returning)
+		{
+			visitor.Output.AppendLine();
+			visitor.Output.Append("RETURNING ");
+
+			if(returning.Fields == null || returning.Fields.Count == 0)
+				visitor.Output.Append("*");
+			else
+			{
+				int index = 0;
+
+				foreach(var field in returning.Fields)
+				{
+					if(index++ > 0)
+						visitor.Output.Append(",");
+
+					visitor.Visit(field);
+				}
+			}
+
+			if(returning.Table != null)
+			{
+				visitor.Output.Append(" INTO ");
+				visitor.Visit(returning.Table);
+			}
+		}
+
+		protected virtual void VisitFrom(IExpressionVisitor visitor, ICollection<ISource> sources)
+		{
+			visitor.VisitFrom(sources, (v, j) => this.VisitJoin(v, j));
+		}
+
+		protected virtual void VisitJoin(IExpressionVisitor visitor, JoinClause joining)
+		{
+			visitor.VisitJoin(joining);
+		}
+
 		protected virtual void VisitWhere(IExpressionVisitor visitor, IExpression where)
 		{
 			visitor.VisitWhere(where);
