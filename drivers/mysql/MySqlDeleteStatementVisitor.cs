97d593cd41f8c0458a6ca79c219dfa7f8f1bfc33
Popeye Zhong <9555843@qq.com>
2019-02-19 11:16:28 +0800

    为MySQL的相关访问器重写了返回子句的生成方法。 :christmas_tree:

--- drivers/mysql/MySqlDeleteStatementVisitor.cs
+++ drivers/mysql/MySqlDeleteStatementVisitor.cs
@@ -50,5 +50,14 @@ namespace Zongsoft.Data.MySql
 		{
 		}
 		#endregion
+
+		#region 重写方法
+		protected override void VisitReturning(IExpressionVisitor visitor, ReturningClause returning)
+		{
+			/*
+			 * 由于 MySQL 的 DELETE 语句不支持 RETURN|RETURNING 子句，故不输出任何内容，且不调用基类同名方法以避免生成错误的语句。
+			 */
+		}
+		#endregion
 	}
 }
