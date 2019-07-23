27c93bf486198487b3b56e34f51550f42fe57638
Popeye Zhong <9555843@qq.com>
2019-07-23 19:32:35 +0800

    增加了数据会话类，准备替换 XXXXXTransaction 类。 :trophy:

---
+++ src/Common/DataSession.cs
@@ -0,0 +1,774 @@
+﻿/*
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
+ *
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2015-2019 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Data.
+ *
+ * Zongsoft.Data is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.Data is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.Data; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zongsoft.Data.Common
+{
+	public class DataSession : IDisposable
+	{
+		#region 常量定义
+		private const int NONE_FLAG = 0;
+		private const int READING_FLAG = 1;
+		private const int COMPLETED_FLAG = 1;
+		#endregion
+
+		#region 私有变量
+		private readonly bool ShareConnectionSupported;
+		#endregion
+
+		#region 私有变量
+		private int _completedFlag;
+		private int _reads;
+		private readonly AutoResetEvent _semaphore;
+		private readonly ConcurrentBag<IDbCommand> _commands;
+		#endregion
+
+		#region 成员字段
+		private IDataSource _source;
+		private DbConnection _connection;
+		private DbTransaction _transaction;
+		private Transactions.Transaction _ambient;
+		#endregion
+
+		#region 构造函数
+		public DataSession(IDataSource source, Zongsoft.Transactions.Transaction ambient = null)
+		{
+			_source = source ?? throw new ArgumentNullException(nameof(source));
+			_ambient = ambient;
+
+			_semaphore = new AutoResetEvent(true);
+			_commands = new ConcurrentBag<IDbCommand>();
+
+			this.ShareConnectionSupported = source.Features.Support(Feature.MultipleActiveResultSets);
+		}
+		#endregion
+
+		#region 公共属性
+		public bool InAmbient
+		{
+			get => _ambient != null;
+		}
+
+		public IDataSource Source
+		{
+			get => _source;
+		}
+
+		public IDbConnection Connection
+		{
+			get => _connection;
+		}
+
+		public IDbTransaction DbTransaction
+		{
+			get => _transaction;
+		}
+
+		/// <summary>
+		/// 获取一个值，指示当前环境事务是否已经完成(提交或回滚)。
+		/// </summary>
+		public bool IsCompleted
+		{
+			get => _completedFlag != NONE_FLAG;
+		}
+
+		/// <summary>
+		/// 获取一个值，指示当前环境事务的数据连接是否正在进行数据读取操作。
+		/// </summary>
+		public bool IsReading
+		{
+			get => _reads != NONE_FLAG;
+		}
+		#endregion
+
+		#region 公共方法
+		/// <summary>
+		/// 创建语句对应的 <see cref="DbCommand"/> 数据命令。
+		/// </summary>
+		/// <param name="statement">指定要创建命令的语句。</param>
+		/// <returns>返回创建的数据命令对象。</returns>
+		public DbCommand Build(Expressions.IStatementBase statement)
+		{
+			return new SessionCommand(this, _source.Driver.CreateCommand(statement));
+		}
+
+		public void Commit()
+		{
+			/*
+			 * 注：环境事务的提交由 Enlistment 的回调函数处理。
+			 */
+			if(_ambient == null)
+				this.Complete(true);
+		}
+
+		public void Rollback()
+		{
+			/*
+			 * 注：环境事务的回滚由 Enlistment 的回调函数处理。
+			 */
+			if(_ambient == null)
+				this.Complete(false);
+		}
+
+		public void Dispose()
+		{
+			this.Complete(false);
+		}
+
+		private bool Complete(bool? commit)
+		{
+			//设置完成标记
+			var completed = Interlocked.Exchange(ref _completedFlag, COMPLETED_FLAG);
+
+			//如果已经完成过则返回假(False)
+			if(completed == COMPLETED_FLAG)
+				return false;
+
+			//等待信号量
+			_semaphore.WaitOne();
+
+			try
+			{
+				//获取并将数据事务对象置空
+				var transaction = Interlocked.Exchange(ref _transaction, null);
+
+				if(transaction != null && commit.HasValue)
+				{
+					if(commit.Value)
+						transaction.Commit();
+					else
+						transaction.Rollback();
+				}
+
+				//获取并将数据连接对象置空
+				var connection = Interlocked.Exchange(ref _connection, null);
+
+				if(connection != null)
+				{
+					//取消连接事件处理
+					connection.StateChange -= Connection_StateChange;
+
+					//如果当前连接正在读取数据，则不要释放该数据连接（由对应的数据读取器关联的延迟加载集合负责关闭）
+					if(!this.IsReading)
+						connection.Dispose();
+				}
+			}
+			finally
+			{
+				//释放当前持有的信号
+				_semaphore.Set();
+			}
+
+			//释放信号量资源
+			_semaphore.Dispose();
+
+			//返回完成成功
+			return true;
+		}
+		#endregion
+
+		#region 内部方法
+		/// <summary>
+		/// 设置指定命令的数据连接和关联到必要的数据事务。
+		/// </summary>
+		/// <param name="command">指定要绑定的命令对象。</param>
+		internal void Bind(IDbCommand command)
+		{
+			this.Bind(command, () => ShareConnectionSupported || !this.IsReading);
+		}
+
+		private void Bind(IDbCommand command, Func<bool> sharable)
+		{
+			if(command == null)
+				throw new ArgumentNullException(nameof(command));
+
+			if(sharable == null)
+				throw new ArgumentNullException(nameof(sharable));
+
+			//如果已经终止则返回
+			if(_completedFlag == COMPLETED_FLAG || (_ambient != null && _ambient.IsCompleted))
+				throw new DataException("The data session or ambient transaction have been completed.");
+
+			//等待信号量
+			_semaphore.WaitOne();
+
+			try
+			{
+				//如果已经终止则返回
+				if(_completedFlag == COMPLETED_FLAG || (_ambient != null && _ambient.IsCompleted))
+					throw new DataException("The data session or ambient transaction have been completed.");
+
+				if(_connection == null)
+				{
+					lock(_semaphore)
+					{
+						if(_connection == null)
+						{
+							_connection = _source.Driver.CreateConnection(_source.ConnectionString);
+							_connection.StateChange += Connection_StateChange;
+
+							command.Connection = _connection;
+							_commands.Add(command);
+
+							return;
+						}
+					}
+				}
+
+				if(sharable())
+				{
+					command.Connection = _connection;
+
+					//如果当前事务已启动则更新命令否则将命令加入到待绑定集合中
+					if(_transaction == null)
+						_commands.Add(command); //将指定的命令加入到命令集，等待事务绑定
+					else
+						command.Transaction = _transaction;
+				}
+				else
+				{
+					command.Connection = _source.Driver.CreateConnection(_source.ConnectionString);
+				}
+			}
+			finally
+			{
+				//释放当前持有的信号
+				_semaphore.Set();
+			}
+		}
+
+		/// <summary>
+		/// 尝试进入读取临界区。
+		/// </summary>
+		/// <returns>如果成功进入读取状态则返回真(True)，否则返回假(False)。</returns>
+		/// <remarks>
+		/// 	<p>注意：进入成功的操作者必须确保当读取完成时调用<see cref="ExitRead"/>方法以重置读取标记。</p>
+		/// </remarks>
+		internal bool EnterRead(IDbCommand command)
+		{
+			if(ShareConnectionSupported)
+			{
+				Interlocked.Increment(ref _reads);
+				this.Bind(command, () => true);
+				return true;
+			}
+
+			var original = Interlocked.CompareExchange(ref _reads, READING_FLAG, NONE_FLAG);
+
+			if(original == NONE_FLAG)
+			{
+				this.Bind(command, () => true);
+				return true;
+			}
+			else
+			{
+				this.Bind(command, () => false);
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// 退出读取临界区。
+		/// </summary>
+		internal bool ExitRead()
+		{
+			if(ShareConnectionSupported && _reads > 0)
+			{
+				var count = Interlocked.Decrement(ref _reads);
+
+				if(count < 0)
+					Interlocked.Exchange(ref _reads, 0);
+
+				return count == 0;
+			}
+
+			return Interlocked.Exchange(ref _reads, NONE_FLAG) == READING_FLAG;
+		}
+		#endregion
+
+		#region 事件响应
+		private void Connection_StateChange(object sender, StateChangeEventArgs e)
+		{
+			var connection = (DbConnection)sender;
+
+			if(e.CurrentState == ConnectionState.Open)
+			{
+				//取消连接事件处理
+				connection.StateChange -= Connection_StateChange;
+
+				//开启一个数据库事务
+				_transaction = connection.BeginTransaction(GetIsolationLevel());
+
+				//依次设置待绑定命令的事务
+				while(_commands.TryTake(out var command))
+				{
+					command.Transaction = _transaction;
+				}
+
+				if(_ambient != null)
+					_ambient.Enlist(new Enlistment(this));
+			}
+		}
+		#endregion
+
+		#region 私有方法
+		[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+		private IsolationLevel GetIsolationLevel()
+		{
+			if(_ambient == null)
+				return IsolationLevel.Unspecified;
+
+			switch(_ambient.IsolationLevel)
+			{
+				case Transactions.IsolationLevel.ReadCommitted:
+					return IsolationLevel.ReadCommitted;
+				case Transactions.IsolationLevel.ReadUncommitted:
+					return IsolationLevel.ReadUncommitted;
+				case Transactions.IsolationLevel.RepeatableRead:
+					return IsolationLevel.RepeatableRead;
+				case Transactions.IsolationLevel.Serializable:
+					return IsolationLevel.Serializable;
+				default:
+					return IsolationLevel.Unspecified;
+			}
+		}
+		#endregion
+
+		#region 嵌套子类
+		private class Enlistment : Zongsoft.Transactions.IEnlistment
+		{
+			private DataSession _session;
+
+			public Enlistment(DataSession session)
+			{
+				_session = session;
+			}
+
+			public void OnEnlist(Zongsoft.Transactions.EnlistmentContext context)
+			{
+				if(context.Phase == Zongsoft.Transactions.EnlistmentPhase.Prepare)
+					return;
+
+				switch(context.Phase)
+				{
+					case Transactions.EnlistmentPhase.Commit:
+						_session.DbTransaction.Commit();
+						break;
+					case Transactions.EnlistmentPhase.Abort:
+					case Transactions.EnlistmentPhase.Rollback:
+						_session.DbTransaction.Rollback();
+						break;
+				}
+
+				_session.Complete(null);
+			}
+		}
+
+		private class SessionCommand : DbCommand
+		{
+			#region 成员字段
+			private readonly DataSession _session;
+			private readonly DbCommand _command;
+			#endregion
+
+			#region 构造函数
+			internal SessionCommand(DataSession session, DbCommand command)
+			{
+				_session = session ?? throw new ArgumentNullException(nameof(session));
+				_command = command ?? throw new ArgumentNullException(nameof(command));
+			}
+			#endregion
+
+			#region 重写属性
+			public override string CommandText
+			{
+				get => _command.CommandText;
+				set => _command.CommandText = value;
+			}
+
+			public override CommandType CommandType
+			{
+				get => _command.CommandType;
+				set => _command.CommandType = value;
+			}
+
+			public override int CommandTimeout
+			{
+				get => _command.CommandTimeout;
+				set => _command.CommandTimeout = value;
+			}
+
+			protected override DbConnection DbConnection
+			{
+				get => _command.Connection;
+				set => _command.Connection = value;
+			}
+
+			protected override DbTransaction DbTransaction
+			{
+				get => _command.Transaction;
+				set => _command.Transaction = value;
+			}
+
+			protected override DbParameterCollection DbParameterCollection
+			{
+				get => _command.Parameters;
+			}
+
+			public override bool DesignTimeVisible
+			{
+				get => _command.DesignTimeVisible;
+				set => _command.DesignTimeVisible = value;
+			}
+
+			public override UpdateRowSource UpdatedRowSource
+			{
+				get => _command.UpdatedRowSource;
+				set => _command.UpdatedRowSource = value;
+			}
+			#endregion
+
+			#region 重写方法
+			public override void Cancel()
+			{
+				if(_command.Connection != null)
+					_command.Cancel();
+			}
+
+			public override void Prepare()
+			{
+				_command.Prepare();
+			}
+
+			protected override DbParameter CreateDbParameter()
+			{
+				return _command.CreateParameter();
+			}
+
+			public override object ExecuteScalar()
+			{
+				//确认数据连接已打开
+				this.EnsureConnect();
+
+				//返回数据命令执行结果
+				return _command.ExecuteScalar();
+			}
+
+			public override async Task<object> ExecuteScalarAsync(CancellationToken cancellationToken)
+			{
+				//确认数据连接已打开
+				await this.EnsureConnectAsync(cancellationToken);
+
+				//返回数据命令执行结果
+				return await _command.ExecuteScalarAsync(cancellationToken);
+			}
+
+			public override int ExecuteNonQuery()
+			{
+				//确认数据连接已打开
+				this.EnsureConnect();
+
+				//返回数据命令执行结果
+				return _command.ExecuteNonQuery();
+			}
+
+			public override async Task<int> ExecuteNonQueryAsync(CancellationToken cancellationToken)
+			{
+				//确认数据连接已打开
+				await this.EnsureConnectAsync(cancellationToken);
+
+				//返回数据命令执行结果
+				return await _command.ExecuteNonQueryAsync(cancellationToken);
+			}
+
+			protected override DbDataReader ExecuteDbDataReader(CommandBehavior behavior)
+			{
+				//尝试进入数据会话的读取临界区。
+				//如果进入成功，则表明该会话支持多活动结果集(MARS)，或者当前操作是会话中的首个读取请求
+				if(_session.EnterRead(_command))
+				{
+					//确认数据连接已打开
+					this.EnsureConnect();
+
+					//执行数据命令的读取方法，并构建一个会话数据读取器。
+					//注意：该读取器在关闭时会退出读取临界区，并根据会话完成状态确定是否关闭数据连接。
+					return new SessionReader(_session, _command.ExecuteReader(behavior & ~CommandBehavior.CloseConnection));
+				}
+				else
+				{
+					//确认数据连接已打开
+					this.EnsureConnect();
+
+					//读取临界区进入失败：执行得到一个普通的数据读取器，该读取器始终绑定到一个新的数据连接。
+					//注意：该读取器在关闭时会关闭对应的数据连接。
+					return _command.ExecuteReader(behavior | CommandBehavior.CloseConnection);
+				}
+			}
+
+			protected override async Task<DbDataReader> ExecuteDbDataReaderAsync(CommandBehavior behavior, CancellationToken cancellationToken)
+			{
+				//尝试进入数据会话的读取临界区。
+				//如果进入成功，则表明该会话支持多活动结果集(MARS)，或者当前操作是会话中的首个读取请求
+				if(_session.EnterRead(_command))
+				{
+					//确认数据连接已打开
+					await this.EnsureConnectAsync(cancellationToken);
+
+					//执行数据命令的读取方法，并构建一个会话数据读取器。
+					//注意：该读取器在关闭时会退出读取临界区，并根据会话完成状态确定是否关闭数据连接。
+					return new SessionReader(_session, await _command.ExecuteReaderAsync(behavior & ~CommandBehavior.CloseConnection, cancellationToken));
+				}
+				else
+				{
+					//确认数据连接已打开
+					await this.EnsureConnectAsync(cancellationToken);
+
+					//读取临界区进入失败：执行得到一个普通的数据读取器，该读取器始终绑定到一个新的数据连接。
+					//注意：该读取器在关闭时会关闭对应的数据连接。
+					return await _command.ExecuteReaderAsync(behavior | CommandBehavior.CloseConnection, cancellationToken);
+				}
+			}
+			#endregion
+
+			#region 私有方法
+			[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+			private void EnsureConnect()
+			{
+				if(_command.Connection == null)
+					_session.Bind(_command);
+
+				if(_command.Connection.State == ConnectionState.Closed || _command.Connection.State == ConnectionState.Broken)
+					_command.Connection.Open();
+			}
+
+			[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+			private Task EnsureConnectAsync(CancellationToken cancellationToken)
+			{
+				if(_command.Connection == null)
+					_session.Bind(_command);
+
+				if(_command.Connection.State == ConnectionState.Closed || _command.Connection.State == ConnectionState.Broken)
+					return _command.Connection.OpenAsync(cancellationToken);
+
+				return Task.CompletedTask;
+			}
+			#endregion
+		}
+
+		private class SessionReader : DbDataReader
+		{
+			#region 成员字段
+			private readonly DataSession _session;
+			private readonly DbDataReader _reader;
+			private DbConnection _connection;
+			#endregion
+
+			#region 构造函数
+			internal SessionReader(DataSession session, DbDataReader reader)
+			{
+				_session = session ?? throw new ArgumentNullException(nameof(session));
+				_reader = reader ?? throw new ArgumentNullException(nameof(reader));
+				_connection = session._connection;
+			}
+			#endregion
+
+			#region 重写属性
+			public override object this[int ordinal] => _reader[ordinal];
+
+			public override object this[string name] => _reader[name];
+
+			public override int Depth => _reader.Depth;
+
+			public override int FieldCount => _reader.FieldCount;
+
+			public override bool HasRows => _reader.HasRows;
+
+			public override bool IsClosed => _reader.IsClosed;
+
+			public override int RecordsAffected => _reader.RecordsAffected;
+			#endregion
+
+			#region 重写方法
+			public override bool GetBoolean(int ordinal)
+			{
+				return _reader.GetBoolean(ordinal);
+			}
+
+			public override byte GetByte(int ordinal)
+			{
+				return _reader.GetByte(ordinal);
+			}
+
+			public override long GetBytes(int ordinal, long offset, byte[] buffer, int bufferOffset, int length)
+			{
+				return _reader.GetBytes(ordinal, offset, buffer, bufferOffset, length);
+			}
+
+			public override char GetChar(int ordinal)
+			{
+				return _reader.GetChar(ordinal);
+			}
+
+			public override long GetChars(int ordinal, long offset, char[] buffer, int bufferOffset, int length)
+			{
+				return _reader.GetChars(ordinal, offset, buffer, bufferOffset, length);
+			}
+
+			public override DateTime GetDateTime(int ordinal)
+			{
+				return _reader.GetDateTime(ordinal);
+			}
+
+			public override decimal GetDecimal(int ordinal)
+			{
+				return _reader.GetDecimal(ordinal);
+			}
+
+			public override double GetDouble(int ordinal)
+			{
+				return _reader.GetDouble(ordinal);
+			}
+
+			public override float GetFloat(int ordinal)
+			{
+				return _reader.GetFloat(ordinal);
+			}
+
+			public override Guid GetGuid(int ordinal)
+			{
+				return _reader.GetGuid(ordinal);
+			}
+
+			public override short GetInt16(int ordinal)
+			{
+				return _reader.GetInt16(ordinal);
+			}
+
+			public override int GetInt32(int ordinal)
+			{
+				return _reader.GetInt32(ordinal);
+			}
+
+			public override long GetInt64(int ordinal)
+			{
+				return _reader.GetInt64(ordinal);
+			}
+
+			public override string GetString(int ordinal)
+			{
+				return _reader.GetString(ordinal);
+			}
+
+			public override object GetValue(int ordinal)
+			{
+				return _reader.GetValue(ordinal);
+			}
+
+			public override int GetValues(object[] values)
+			{
+				return _reader.GetValues(values);
+			}
+
+			public override string GetName(int ordinal)
+			{
+				return _reader.GetName(ordinal);
+			}
+
+			public override int GetOrdinal(string name)
+			{
+				return _reader.GetOrdinal(name);
+			}
+
+			public override Type GetFieldType(int ordinal)
+			{
+				return _reader.GetFieldType(ordinal);
+			}
+
+			public override string GetDataTypeName(int ordinal)
+			{
+				return _reader.GetDataTypeName(ordinal);
+			}
+
+			public override IEnumerator GetEnumerator()
+			{
+				return _reader.GetEnumerator();
+			}
+
+			public override bool IsDBNull(int ordinal)
+			{
+				return _reader.IsDBNull(ordinal);
+			}
+
+			public override bool NextResult()
+			{
+				return _reader.NextResult();
+			}
+
+			public override bool Read()
+			{
+				return _reader.Read();
+			}
+
+			public override void Close()
+			{
+				//关闭数据读取器
+				_reader.Close();
+
+				//退出环境事务的读取临界区（即重置环境事务的读取标记）
+				var disconnectRequired = _session.ExitRead();
+
+				/*
+				 * 如果数据会话已经完结，则需要关闭释放对应的数据连接。
+				 * 因为当会话完成(提交或回滚)时，如果该会话正处于读取状态(即位于读取临界区内)，
+				 * 完成操作是不会关闭数据连接的，因为读取操作还需要使用它，即该数据连接由本读取器进行关闭。
+				 */
+				if(disconnectRequired && _session.IsCompleted)
+				{
+					var connection = _connection;
+
+					if(connection != null)
+						connection.Dispose();
+				}
+			}
+			#endregion
+		}
+		#endregion
+	}
+}
