3cc91ef5b87f99efd15045351548a065eafe0b53
Popeye Zhong <9555843@qq.com>
2019-07-25 00:13:35 +0800

    修复MySQL驱动程序中的Update语句访问器的缺陷。 :bouquet: :saxophone:

--- drivers/mysql/MySqlUpdateStatementVisitor.cs
+++ drivers/mysql/MySqlUpdateStatementVisitor.cs
@@ -52,14 +52,87 @@ namespace Zongsoft.Data.MySql
 		#endregion
 
 		#region 重写方法
-		protected override void VisitFrom(IExpressionVisitor visitor, ICollection<ISource> sources)
+		protected override void VisitTables(IExpressionVisitor visitor, IList<TableIdentifier> tables, UpdateStatement statement)
+		{
+			//调用基类同名方法
+			base.VisitTables(visitor, tables, statement);
+
+			/*
+			 * 注意：由于 MySQL 的 UPDATE 语句不支持 FROM 子句，因此必须将其改写为多表修改的语法。
+			 */
+
+			if(statement.HasFrom)
+			{
+				foreach(var source in statement.From)
+				{
+					switch(source)
+					{
+						case TableIdentifier table:
+							if(!tables.Contains(table))
+							{
+								visitor.Output.Append(",");
+								visitor.Visit(table);
+							}
+
+							break;
+						case JoinClause join:
+							if(join.Target is TableIdentifier target)
+							{
+								visitor.Output.Append(",");
+								visitor.Visit(target);
+							}
+							else
+							{
+								throw new DataException($"The {MySqlDriver.Key} driver does not support the FROM clause of the UPDATE statement contain an expression of type '{join.Target.GetType().Name}'.");
+							}
+
+							break;
+						default:
+							throw new NotSupportedException($"The {MySqlDriver.Key} driver does not support the FROM clause of the UPDATE statement contain an expression of type '{source.GetType().Name}'.");
+					}
+				}
+			}
+		}
+
+		protected override void VisitWhere(IExpressionVisitor visitor, IExpression where, UpdateStatement statement)
+		{
+			/*
+			 * 注意：由于 MySQL 的 UPDATE 语句不支持 FROM 子句，因此必须将其改写为多表修改的语法。
+			 * 由于 FROM 子句中可能包含 JOIN 类型语句，所以必须将 JOIN 子句中的条件式添加到 UPDATE 语句的 WHERE 子句中。
+			 */
+
+			if(statement.HasFrom)
+			{
+				var expressions = Expression.Block(BlockExpressionDelimiter.Space);
+
+				foreach(var source in statement.From)
+				{
+					if(source is JoinClause join)
+					{
+						expressions.Add(join.Type == JoinType.Inner ? Expression.Literal("AND") : Expression.Literal("OR"));
+						expressions.Add(join.Condition);
+					}
+				}
+
+				if(expressions.Count > 0)
+				{
+					expressions.Insert(0, where);
+					where = expressions;
+				}
+			}
+
+			//调用基类同名方法
+			base.VisitWhere(visitor, where, statement);
+		}
+
+		protected override void VisitFrom(IExpressionVisitor visitor, ICollection<ISource> sources, UpdateStatement statement)
 		{
 			/*
 			 * 由于 MySQL 的 UPDATE 语句不支持 FROM 子句，故不输出任何内容，且不调用基类同名方法以避免生成错误的语句。
 			 */
 		}
 
-		protected override void VisitReturning(IExpressionVisitor visitor, ReturningClause returning)
+		protected override void VisitReturning(IExpressionVisitor visitor, ReturningClause returning, UpdateStatement statement)
 		{
 			/*
 			 * 由于 MySQL 的 UPDATE 语句不支持 RETURN|RETURNING 子句，故不输出任何内容，且不调用基类同名方法以避免生成错误的语句。
